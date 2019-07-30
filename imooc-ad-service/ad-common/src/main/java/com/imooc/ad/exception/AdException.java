0aa1f5449efaccd7be866b51d27f70b8f816321a
18838988676@163.com <git18838988676>
2019-07-30 09:28:55 +0800

    前5天

---
+++ imooc-ad-service/ad-common/src/main/java/com/imooc/ad/exception/AdException.java
@@ -0,0 +1,12 @@
+package com.imooc.ad.exception;
+
+/**
+ * Created by Qinyi.
+ */
+//定义一个异常类
+public class AdException extends Exception {
+
+    public AdException(String message) {
+        super(message);
+    }
+}
